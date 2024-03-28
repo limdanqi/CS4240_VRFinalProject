3af6ebdf03541be0e224e08e3ceabcee3d729bb1
limdanqi <limdanqi01@gmail.com>
2024-03-28 16:39:27 +0800

    Add edited teleport prefabs

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -5,13 +5,13 @@ using UnityEngine.InputSystem;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
-    public GameObject leftTeleportation;
+    public GameObject teleportationRay;
 
-    public InputActionProperty leftActivate;
+    public InputActionProperty inputAction;
 
     void Update()
     {
-        // Activates left teleportation ray if input key is pressed
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
+        // Activates teleportation ray if input key is pressed
+        teleportationRay.SetActive(inputAction.action.ReadValue<float>() > 0.1f);
     }
 }

69f4baceb6ce5750df4e26da73ae525c69aa6ee5
Yiyang <liuyiyang0929@gmail.com>
2024-04-03 22:09:31 +0800

    Add animations for animals to react to right/wrong habitat

---
+++ Assets/Scripts/AnimalAnimationController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalAnimationController : MonoBehaviour
+{
+    Animator animator;
+    string tag;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        tag = gameObject.tag + "area";
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag(tag))
+        {
+            animator.SetBool("isWrongHabitat", false);
+        } else
+        {
+            animator.SetBool("isWrongHabitat", true);
+        }
+    }
+}

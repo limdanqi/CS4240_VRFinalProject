3468c6d7bd65df1ae81be36183ebe3f69e2de35c
liviamil <liviamil@u.nus.edu>
2024-04-08 10:15:42 +0800

    Add respawn point for non-aquatic animals

---
+++ Assets/Scripts/GameRespawn.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRespawn : MonoBehaviour
+{
+    private Vector3 respawnPosition1 = new Vector3(106.45f, 2.026628f, 138.7f);
+    private Vector3 respawnPosition2 = new Vector3(123.37f, 2.026628f, 125.74f);
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "lakearea")
+        {
+            Vector3 respawnPosition = transform.position.x < 120 ? respawnPosition1 : respawnPosition2;
+            transform.position = respawnPosition;
+        }
+    }
+}
